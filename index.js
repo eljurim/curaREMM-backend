c3dee74c3ae8c7047cf3b5a09521abd2c027a97c
Manuel Eljuri <manueleljuri@gmail.com>
2018-12-21 12:47:34 -0600

    por el puerco de oro

--- index.js
+++ index.js
@@ -9,6 +9,28 @@ const Doctor = require('./Doctors').model
 const Hospital = require('./Hospitals').model
 db.connect()
 
+function calculateDistance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist;
+	}
+}
+
 let idAndLocationArray = [{"id":"5bfd85136004132880c3b8ee","latitude":"19.4376855","longitude":"-99.19358269999998"},{"id":"5bfd85126004132880c3b8e0","latitude":"19.2773291","longitude":"-99.12248499999998"},{"id":"5bfd85136004132880c3b8ed","latitude":"19.4376855","longitude":"-99.19358269999998"},{"id":"5bfd85136004132880c3b8e7","latitude":"19.4914349","longitude":"-99.24406809999999"},{"id":"5bfd85136004132880c3b8ec","latitude":"19.4376855","longitude":"-99.19358269999998"},{"id":"5bfd85146004132880c3b8f7","latitude":"19.5342207","longitude":"-99.2257573"},{"id":"5bfd85146004132880c3b8fc","latitude":"19.4386598","longitude":"-99.1551556"},{"id":"5bfd85146004132880c3b8fd","latitude":"19.3834269","longitude":"-99.16588469999999"},{"id":"5bfd85146004132880c3b900","latitude":"19.3279731","longitude":"-99.1436114"},{"id":"5bfd85146004132880c3b8f8","latitude":"19.5342207","longitude":"-99.2257573"},{"id":"5bfd85206004132880c3b9cf","latitude":"19.2842868","longitude":"-99.67375609999999"},{"id":"5bfd85226004132880c3b9f2","latitude":"19.4937311","longitude":"-99.2464448"},{"id":"5bfd85216004132880c3b9ed","latitude":"19.4172254","longitude":"-99.17147349999999"},{"id":"5bfd85226004132880c3b9f7","latitude":"21.5041651","longitude":"-104.89458869999999"},{"id":"5bfd850f6004132880c3b89f","latitude":"19.4240109","longitude":"-99.10491389999999"},{"id":"5bfd850f6004132880c3b89e","latitude":"19.4240109","longitude":"-99.10491389999999"}]
 
 app.use(bodyParser.json());
@@ -36,10 +58,24 @@ app.post('/doctors/add', async (req ,resp)=>{
     await Doctor.create(req.body)
     resp.send(req.body.name + " Creado")
 })
-
+// a modificar para geolocalizacion
 app.post('/doctors/findBySpecialty', async (req ,resp)=>{
     console.log(req.body);
-    resp.send(await Doctor.find(req.body))
+    let responseWithDistance = []
+    // let jsonBody = JSON.parse(req.body.position)
+    console.log(req.body.latitude)
+    console.log(req.body.longitude)
+    await Doctor.find({"specialties":req.body.specialties},(err, foundDoctors) =>{
+        foundDoctors.forEach((currentDoctor)=>{
+            let distance = calculateDistance(req.body.latitude, req.body.longitude, currentDoctor.workPlaces.latitude, currentDoctor.workPlaces.longitude, "K")
+            console.log(distance)
+            currentDoctor = (null,foundDoctors)
+            currentDoctor = Object.assign({distance},currentDoctor)
+            responseWithDistance.push(currentDoctor)
+        })
+        
+        resp.send(responseWithDistance)
+    })
 })
 
 app.get('/doctors/specialtiesList', async (req, resp)=>{
